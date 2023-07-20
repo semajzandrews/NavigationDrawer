8cb5a54d372267548b2d10886b926c946e027589
Semaj Andrews <officialmaj@Semajs-MacBook-Pro-2.local>
2023-07-20 13:26:30 -0400

    Project Created

---
+++ NavigationDrawer/NavigationDrawer/NavigationDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDrawerApp.swift
+//  NavigationDrawer
+//
+//  Created by Semaj Andrews on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
